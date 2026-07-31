10d1a3004a3ed74f198f3100eefbdc4f38574d30
Vinuthads <104154935+Vinuthads@users.noreply.github.com>
2026-07-31 10:58:54 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,7 +8,7 @@ variable "aws_region" {
 variable "instance_type" {
   description = "Free tier EC2 instance type"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
 
 
