e0c420bca3710098c98743e53db90f44a6998a47
Vinuthads <104154935+Vinuthads@users.noreply.github.com>
2026-07-31 18:48:47 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,7 +4,7 @@ terraform {
     key            = "devops/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
-    use_lockfile = "terraform-lock"
+    use_lockfile = true
   }
 
   required_providers {
